2c3ef62f49cc9d4ccbe8fe6f87a9b81e0bdb6dc1
NullAssetsGit <nullsol@tutamailcom>
2026-04-24 11:15:40 +0200

    Fix i18next + vite build

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,17 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+﻿# Обновить vite.config.js:
+'import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
-  
-
-  define: {
-    global: 'globalThis',
-    'process.env': {}
-  },
-
-  
-  build: {
-    rollupOptions: {
+  build: { 
+    rollupOptions: { 
       external: [
-        
-        'rpc-websockets',
-        'rpc-websockets/dist/lib/client',
-        'rpc-websockets/dist/lib/client/websocket.browser',
-        'rpc-websockets/dist/lib/client/websocket.node',
-        
-        
-        'buffer',
-        'crypto',
-        'stream',
-        'events',
-        'util',
-        'assert',
-        
-       
-        '@solana/web3.js/nodewalletAdapter'
-      ]
-    },
-    target: 'esnext' 
-  },
-
- 
-  optimizeDeps: {
-    exclude: [
-      'rpc-websockets',
-      '@solana/web3.js'
-    ]
-  },
-
-  
-  resolve: {
-    alias: {
-      
-      'buffer': 'buffer'
-    }
+        "rpc-websockets",
+        "i18next",
+        "react-i18next"
+      ] 
+    } 
   }
-})
+})' | Out-File -FilePath vite.config.js -Encoding utf8
+
+npm run build
